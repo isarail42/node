01ac5ce376b7eabff12f9ceda3055a3de6f22aab
MR NOOB HERE ! <novana.xyz@gmail.com>
2024-11-26 22:04:51 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from flask import Flask, request
 
 app = Flask(__name__)
-
+token_list = []
 @app.route('/post')
 def main():
+    global token_list
     token = request.args.get('token')
-    open('token.text','w').write(token)
+    token_list.append(token)
     return 'successfull'
 
 @app.route('/get')
 def main_():
-    path = 'token.text'
-    content = open(path,'r').read()
-    if int(len(content)) > 2:
-        open(path,'w').write('')
-        return content
+    global token_list
+    if not str(len(token_list)) == "0":
+       for get_one in token_list:
+           token_list.remove(get_one)
+           break
+       return str(get_one)
     else:
-        return 'None'
+       return 'None'
+@app.route('/total')
+def total():
+    global token_list
+    return f"{token_list} \n\nTotal:  {str(len(token_list))} "
 app.run()
